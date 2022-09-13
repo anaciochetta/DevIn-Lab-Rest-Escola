cc8d3b0074a47349cb130d017b464e84784d8503
aciochetta <anaciochetta@gmail.com>
2022-09-13 16:12:27 -0300

    fix: Erro de conexão com o banco de dados

--- src/Escola.Api/Controllers/BoletinsController.cs
+++ src/Escola.Api/Controllers/BoletinsController.cs
@@ -17,16 +17,5 @@ namespace Escola.Api.Controllers
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
     }
 }
